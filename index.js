1f0f99498a898e55df1d55925a96d96aca341672
RRN1211 <47360131+RRN1211@users.noreply.github.com>
2019-06-25 17:33:05 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var squarei= require("./square");
-const lado=4;
-var a = squarei.areae(lado);
-var p = squarei.perimetroe(lado);
-
-console.log("el area es "+a);
-console.log("el perimetro es "+p);
