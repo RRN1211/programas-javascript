131d4ead276f150b2890f1fcbb8c296e0dc68cd1
RRN1211 <47360131+RRN1211@users.noreply.github.com>
2019-06-25 12:50:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var squarei= require("./square");
+const lado=4;
+var a = squarei.areae(lado);
+var p = squarei.perimetroe(lado);
+
+console.log("el area es "+a);
+console.log("el perimetro es "+p);
---
+++ square.js
@@ -0,0 +1,8 @@
+function area(width){
+return width*width;
+}
+function perimetro(width){
+    return width*4;
+}
+module.exports.areae=area;
+module.exports.perimetroe=perimetro;
